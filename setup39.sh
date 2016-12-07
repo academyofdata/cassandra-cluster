d80431f5e0e51b2a4368c451232572521fe09630
felix <felix@netopia.ro>
2016-12-07 19:28:59 +0200

    script for v3.9

---
+++ setup39.sh
@@ -0,0 +1,6 @@
+echo "deb http://www.apache.org/dist/cassandra/debian 39x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
+sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
+sudo apt-get update
+sudo apt-get install cassandra
+
