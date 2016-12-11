d740d45776a8641d708a3bc21b8bbcbcc45fbdfb
felix <felix@netopia.ro>
2016-12-11 20:08:28 +0200

    script to add replicas

---
+++ gcloud-add-replicas.sh
@@ -0,0 +1,19 @@
+PROJECT="project-id-here"
+ZONE="europe-west1-d"
+MACHINE="g1-small"
+NODE="cass2"
+SEED="cass1"
+
+gcloud compute instances create $NODE --zone $ZONE --machine-type $MACHINE --network "default"  --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" --image "/ubuntu-os-cloud/ubuntu-1604-xenial-v20161205" --boot-disk-size "10" --boot-disk-type "pd-standard" --boot-disk-device-name "$NODEd1" --project $PROJECT
+
+echo "waiting for the machine to come up"
+sleep 30
+
+gcloud compute ssh --project $PROJECT $NODE --zone $ZONE --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/setup39.sh | bash" 
+
+
+IPADDR=`gcloud compute instances list --project $PROJECT --format=text $NODE | grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g'`
+SEEDIP=`gcloud compute instances list --project $PROJECT --format=text $SEED | grep '^networkInterfaces\[[0-9]\+\]\.networkIP:' | sed 's/^.* //g'`
+
+echo "configuring cassandra after installation with $IPADDR and seed $SEEDIP"
+gcloud compute ssh --project $PROJECT $NODE --zone $ZONE --command "wget -qO- https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/config-cassandra.sh | bash -s $IPADDR $SEEDIP"
