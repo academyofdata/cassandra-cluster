a6d01c37bcdb6a4d66df7c8b8dd3d5755b8d9228
fixone <fixone@users.noreply.github.com>
2017-07-26 21:50:11 +0300

    Update get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#just to be safe, try to install wget if not already installed
+apt-get update
+apt-get install -y wget
+#now use wget to download the files
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/movies.csv -O /tmp/movies.csv
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv -O /tmp/ratings_s.csv
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz -O /tmp/ratings.csv.gz
