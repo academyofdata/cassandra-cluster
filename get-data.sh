809025b3dbaabfec2947e09abe62b5d0ae0995a6
fixone <fixone@users.noreply.github.com>
2017-07-26 21:38:39 +0300

    Update get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -4,3 +4,5 @@ wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv 
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz -O /tmp/ratings.csv.gz
 gunzip /tmp/ratings.csv.gz
 wget https://raw.githubusercontent.com/academyofdata/inputs/master/users.csv -O /tmp/users.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata.csv -O /tmp/weatherdata.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata-ext.csv -O /tmp/weatherdata-ext.csv
