815c23ca9271c412a0566eb4072725a942e2ee50
fixone <fixone@users.noreply.github.com>
2017-07-26 21:36:34 +0300

    Create get-data.sh

---
+++ get-data.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/movies.csv -O /tmp/movies.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv -O /tmp/ratings_s.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz -O /tmp/ratings.csv.gz
+gunzip /tmp/ratings.csv.gz
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/users.csv -O /tmp/users.csv
