7ca9e03ffcf34a4ad3aabc86c3315e41f4e4935d
fixone <fixone@users.noreply.github.com>
2017-09-24 13:25:03 +0300

    Update get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+ODIR="/tmp"
+mkdir /data 2>>/dev/null
+if [ -d "/data" ]; then
+        ODIR="/data"
+fi
 # use wget to download the files
-wget https://raw.githubusercontent.com/academyofdata/inputs/master/movies.csv -O /tmp/movies.csv
-wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv -O /tmp/ratings_s.csv
-wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz -O /tmp/ratings.csv.gz
-gunzip -f /tmp/ratings.csv.gz
-wget https://raw.githubusercontent.com/academyofdata/inputs/master/users.csv -O /tmp/users.csv
-wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata.csv -O /tmp/weatherdata.csv
-wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata-ext.csv -O /tmp/weatherdata-ext.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/movies.csv -O $ODIR/movies.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv -O $ODIR/ratings_s.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz -O $ODIR/ratings.csv.gz
+gunzip -f $ODIR/ratings.csv.gz
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/users.csv -O $ODIR/users.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata.csv -O $ODIR/weatherdata.csv
+wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata-ext.csv -O $ODIR/weatherdata-ext.csv
