4168e2160adf108d4cd2ecdf8dcbe213ff2905e8
fixone <fixone@users.noreply.github.com>
2017-10-11 09:20:21 +0300

    Update setup39.sh

--- setup39.sh
+++ setup39.sh
@@ -1,7 +1,7 @@
 sudo add-apt-repository ppa:webupd8team/java
 sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
-echo "deb http://www.apache.org/dist/cassandra/debian 39x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
 sudo apt-get update
