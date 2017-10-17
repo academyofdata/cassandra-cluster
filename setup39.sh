f546eabf335f0b22ee93804847a4e53107da0c8f
fixone <fixone@users.noreply.github.com>
2017-10-17 08:46:16 +0300

    Update setup39.sh

--- setup39.sh
+++ setup39.sh
@@ -5,6 +5,6 @@ echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /
 curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
 sudo apt-get update
-sudo apt-get install cassandra
-sudo apt-get install cassandra-tools
+sudo apt-get install -y cassandra
+sudo apt-get install -y cassandra-tools
 
