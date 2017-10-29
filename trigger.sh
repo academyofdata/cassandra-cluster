7e6b4d74bdf75ea56e9c322892dbe7757a699d23
fixone <fixone@users.noreply.github.com>
2017-10-29 22:20:19 +0200

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,2 +1,2 @@
-wget https://raw.githubusercontent.com/apache/cassandra/trunk/examples/triggers/conf/AuditTrigger.properties
-wget https://raw.githubusercontent.com/apache/cassandra/trunk/examples/triggers/conf/AuditTrigger.properties
+wget -O /etc/cassandra/triggers/trigger-example.jar https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/bin/trigger-example.jar 
+wget -O /etc/cassandra/conf/AuditTrigger.properties https://raw.githubusercontent.com/apache/cassandra/trunk/examples/triggers/conf/AuditTrigger.properties 
