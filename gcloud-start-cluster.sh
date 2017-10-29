07930d1ae67bd0028dff1e2a18be9b2b3df04e75
fixone <fixone@users.noreply.github.com>
2017-10-29 14:41:55 +0200

    Create gcloud-start-cluster.sh

---
+++ gcloud-start-cluster.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo -n "How many nodes? "
+read numnodes
+if ! [ "$numnodes" -eq "$numnodes" ] 2> /dev/null
+then
+    echo "Please enter an integer next time"
+    exit
+fi
+SDIR=$(dirname $0)
+$SDIR/gcloud-add-server.sh node01
+for ((i=2;i<=$numnodes;i++))
+do
+        echo "starting node0$i"
+        $SDIR/gcloud-add-replicas.sh node0$i
+done
