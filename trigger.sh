f6a8234e7e9b2084125f85a77f7f66e5984a19de
fixone <fixone@users.noreply.github.com>
2017-10-29 22:25:11 +0200

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo wget -O /etc/cassandra/triggers/trigger-example.jar https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/bin/trigger-example.jar
 if [ ! -d "/etc/cassandra/conf/" ]; then
-        mkdir /etc/cassandra/conf/
+        sudo mkdir /etc/cassandra/conf/
 fi
 sudo wget -O /etc/cassandra/conf/AuditTrigger.properties https://raw.githubusercontent.com/apache/cassandra/trunk/examples/triggers/conf/AuditTrigger.properties 
