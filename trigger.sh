bf52d4d5f862dbd9438f02aca990ec213d03a431
fixone <fixone@users.noreply.github.com>
2017-10-30 10:40:08 +0200

    Update trigger.sh

--- trigger.sh
+++ trigger.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#example for cassandra triggers as explained here https://github.com/apache/cassandra/tree/trunk/examples/triggers
 sudo wget -O /etc/cassandra/triggers/trigger-example.jar https://raw.githubusercontent.com/academyofdata/cassandra-cluster/master/bin/trigger-example.jar
 if [ ! -d "/etc/cassandra/conf/" ]; then
         sudo mkdir /etc/cassandra/conf/
