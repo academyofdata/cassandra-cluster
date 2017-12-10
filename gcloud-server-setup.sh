bd68d5bde64b1e834927b9b8ee7ea281f2229fca
fixone <fixone@users.noreply.github.com>
2017-12-10 13:37:11 +0200

    Update gcloud-server-setup.sh

--- gcloud-server-setup.sh
+++ gcloud-server-setup.sh
@@ -9,7 +9,7 @@ fi
 
 echo "using ${NODE} as instance name"
 
-gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "cassandra-seed=true"
+gcloud compute instances create ${NODE} --zone ${ZONE} --machine-type ${MACHINE} --maintenance-policy "MIGRATE" --image "https://www.googleapis.com/compute/v1/projects/ubuntu-os-cloud/global/images/family/ubuntu-1604-lts" --boot-disk-size "50" --boot-disk-type "pd-standard" --boot-disk-device-name "${NODE}disk" --labels "cassandra-seed=true"
 echo "waiting for the machine to come up"
 sleep 30
 
