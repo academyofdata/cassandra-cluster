6ef46e18df6fec5f6d3f7c0c4e562dc4149392d0
Felix <fixone@users.noreply.github.com>
2019-10-15 22:42:39 +0300

    Update get-data.sh

--- get-data.sh
+++ get-data.sh
@@ -8,7 +8,11 @@ fi
 sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/movies.csv -O $ODIR/movies.csv
 sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings2.csv -O $ODIR/ratings_s.csv
 sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/ratings.csv.gz -O $ODIR/ratings.csv.gz
+sudo wget https://raw.githubusercontent.com/academyofdata/data/master/trx.csv.gz -O $ODIR/trx.csv.gz
+
 sudo gunzip -f $ODIR/ratings.csv.gz
+sudo gunzip -f $ODIR/trx.csv.gz
 sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/users.csv -O $ODIR/users.csv
 sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata.csv -O $ODIR/weatherdata.csv
+sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata-ext.csv -O $ODIR/weatherdata-ext.csv
 sudo wget https://raw.githubusercontent.com/academyofdata/inputs/master/weatherdata-ext.csv -O $ODIR/weatherdata-ext.csv
