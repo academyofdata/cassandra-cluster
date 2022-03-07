0910fea92c1369a4dba1106f6a578d9e840c3325
Felix <fixone@users.noreply.github.com>
2022-03-07 21:45:01 +0200

    add repo key E91335D77E3E87CB

--- setup39.sh
+++ setup39.sh
@@ -4,6 +4,7 @@ echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | su
 echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key A278B781FE4B2BDA
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key E91335D77E3E87CB
 sudo apt-get update
 sudo apt-get install --allow-unauthenticated -y cassandra cassandra-tools 
  
