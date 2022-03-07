6d7599d1afb8b232cfffc9f6f4ee097988430d76
Felix <fixone@users.noreply.github.com>
2022-03-07 21:38:31 +0200

    change keyserver

--- setup39.sh
+++ setup39.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 echo oracle-java8-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 echo "deb http://www.apache.org/dist/cassandra/debian 311x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
 curl https://www.apache.org/dist/cassandra/KEYS | sudo apt-key add -
-sudo apt-key adv --keyserver pool.sks-keyservers.net --recv-key A278B781FE4B2BDA
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-key A278B781FE4B2BDA
 sudo apt-get update
 sudo apt-get install --allow-unauthenticated -y cassandra cassandra-tools 
  
